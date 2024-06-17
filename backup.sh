a10432154e61d1c9afc5f268df2a6fa36cbf6574
DomenicoVillari3 <domenico.villari3@gmail.com>
2024-06-17 11:06:39 +0200

    last I hope

--- backup.sh
+++ backup.sh
@@ -1,41 +1,50 @@
 #!/bin/bash
 
-#Var utili 
+# Var utili 
 ISCSI_TARGET="iqn.2024-01.example.com:lun1" 
-ISCSI_PORTAL="192.168.128.232" #ip tgt
+ISCSI_PORTAL="192.168.128.232" # ip tgt
 MOUNT_POINT="/mnt"
 CONFIG_FILE="/etc/rsnapshot.conf"
-DEVICE="/dev/sdc" 
+
+# Logout da iSCSI con reindirizzamento su stderr
+sudo iscsiadm --mode node --targetname $ISCSI_TARGET --portal $ISCSI_PORTAL --logout > /dev/null 2>&1
+
+# Login  iSCSI
+sudo iscsiadm --mode node --targetname $ISCSI_TARGET --portal $ISCSI_PORTAL --login > /dev/null 2>&1
+#recupera statop di ritorno dell'ultimo comando
+login_status=$?
+sleep 3
+
+#definizione del disco (sdc o sdd) 
+DEVICE="/dev/"
+DEVICE+=$(lsblk -n -o NAME | grep -E '^sd[c,d]$')
 
 
-sudo iscsiadm --mode node --targetname $ISCSI_TARGET --portal $ISCSI_PORTAL --logout
 
-
-sudo iscsiadm --mode node --targetname $ISCSI_TARGET --portal $ISCSI_PORTAL --login
-login_status = $?
-sleep 5
-
-# Mount sulla partizione ISCSI
-sudo mount $DEVICE $MOUNT_POINT
-
-if $login_status -ne 0; then
-    echo "errore nella connessione al server iscsi."
+# Verifica se il login è riuscito (0 se è andato a buon fine)
+if [ $login_status -ne 0 ]; then
+    echo "Errore nella connessione al server iSCSIa alle $(date +"%d-%m-%Y %H:%M")"
+    exit 1
 else 
-    # Verifica mount
-    if mount | grep $MOUNT_POINT > /dev/null; then
+    # Mount sulla partizione ISCSI
+    sudo mount $DEVICE $MOUNT_POINT
+    #stato di uscita del mount 
+    mount_status=$?
 
-        #esecuzione del backup
-        sudo rsnapshot -c $CONFIG_FILE sync
-
-        sleep 5
-
-        #umount
-        sudo umount $MOUNT_POINT
-
-        sudo iscsiadm --mode node --targetname $ISCSI_TARGET --portal $ISCSI_PORTAL --logout
-
-        echo  "backup terminato il $(date +"%d-%m-%Y %H:%M")"
+    # Verifica se il mount è riuscito
+    if [ $mount_status -ne 0 ]; then
+        echo "Errore: il file system non è stato montato correttamente alle $(date +"%d-%m-%Y %H:%M")"
+        exit 1
     else
-        echo "Errore: il file system non è stato montato correttamente."
+        # Esecuzione del backup
+        sudo rsnapshot -c $CONFIG_FILE sync
+        sleep 3
+
+        # Umount
+        sudo umount $MOUNT_POINT
+        sudo iscsiadm --mode node --targetname $ISCSI_TARGET --portal $ISCSI_PORTAL --logout > /dev/null 2>&1
+
+        echo "Backup terminato il $(date +"%d-%m-%Y %H:%M")"
+        
     fi
 fi
